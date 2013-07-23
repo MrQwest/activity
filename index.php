01f70b2e757681c1ef445969aae8bb90a587c5f6
mrqwest <mrqwest@gmail.com>
2013-07-23 10:17:11 +0100

    last chance saloon

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
 	<meta name="viewport" content="width=device-width, target-densitydpi=160dpi, initial-scale=1">
 	<meta name="apple-mobile-web-app-capable" content="yes" />
 	<link rel="icon" type="image/png" href="favicon.png" />
-	<link rel="apple-touch-icon" sizes="144x144" href="touch-icon-ipad-retina.png" />
+	<link rel="apple-touch-icon" sizes="114x114" href="touch-icon-iphone-retina.png" />
 
 	<link rel="stylesheet" type="text/css" href="css/style.css" media="all">
 </head>
